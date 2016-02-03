09cf017376026e9e88b73ab0862c6a3bf8d7a7b9
Kris Szafranski <kdszafranski@gmail.com>
2016-02-03 14:21:03 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	// Event Listeners
+	$('#fadeOut').on('click', function() {
+		$('#murray').fadeOut(500);
+	});
+
+	$('#fadeIn').on('click', function() {
+		$('#murray').fadeIn(500);
+	});
+
+});
