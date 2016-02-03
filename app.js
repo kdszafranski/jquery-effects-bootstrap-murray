8d68062b332341b23586aea255e50cf7fc0d8dff
Kris Szafranski <kdszafranski@gmail.com>
2016-02-03 14:47:24 -0600

    updated id name scheme, changed murray imgs to classes. now all buttons affect all images

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 $(document).ready(function() {
+	var img = '<img class="murray" src="http://www.fillmurray.com/250/250" />';
+
 	// Event Listeners
-	$('#fadeOut').on('click', function() {
-		$('#murray').fadeOut(500);
+	$('#fade-out').on('click', function() {
+		$('.murray').fadeOut(500);
 	});
 
-	$('#fadeIn').on('click', function() {
-		$('#murray').fadeIn(500);
+	$('#fade-in').on('click', function() {
+		$('.murray').fadeIn(500);
+	});
+
+	$('#remove-button').on('click', function() {
+		// removes ALL my murrays from the page
+		$('.murray').remove();
+	});
+
+	$('#add-button').on('click', function() {
+		$('#murray-box').append(img);
 	});
 
 });
+
+
+function randomNumber() {
+
+}
