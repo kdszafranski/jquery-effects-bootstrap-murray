964077aa7be54009e8f60598f53f29a5b2898639
Kris Szafranski <kdszafranski@gmail.com>
2016-02-03 16:44:01 -0600

    added image selection with add/removeclass. effects still affect all images

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+	
+	var counter = 0;
 
 	// Event Listeners
 	$('#fade-out').on('click', function() {
@@ -13,6 +15,14 @@ $(document).ready(function() {
 		$('.murray').fadeOut(500).fadeIn(250);
 	});
 
+	$('#slide-up').on('click', function() {
+		$('.murray').slideUp();
+	});
+
+	$('#slide-down').on('click', function() {
+		$('.murray').slideDown("slow");
+	});
+
 	$('#remove-button').on('click', function() {
 		// removes ALL murrays from the page
 		$('.murray').remove();
@@ -22,9 +32,31 @@ $(document).ready(function() {
 		var size = randomNumber(200, 500);
 		var img = '<img class="murray" src="http://www.fillmurray.com/' + size + '/' + size + '" />';
 		$('#murray-box').append(img);
+		$('#murray-box').children().last().data('id', counter);
+		console.log(counter);
+		counter++;
 	});
 
+	$('#murray-box').on('click', '.murray', selectMurray);
+
 });
+
+function selectMurray() {
+	var selected = $(this).data('id');
+	console.log(selected);
+	
+	$('.murray').each(function(i) {
+		var id = $(this).data('id');
+		if(id == selected) {
+			$(this).data('selected', true);
+			$(this).addClass('selected');
+		} else {
+			$(this).data('selected', false);
+			$(this).removeClass('selected');
+		}
+		console.log($(this).data());
+	});
+}
 
 
 function randomNumber(min, max){
