9a7614e98590219f9045c521cacd92350af31703
Kris Szafranski <kdszafranski@gmail.com>
2016-08-08 15:19:02 -0500

    updated from lecture

--- app.js
+++ app.js
@@ -1,44 +1,37 @@
 $(document).ready(function() {
-	// Event Listeners
-  $('#fade-out').on('click', function() {
-    $('.murray').fadeOut(500);
+  // effects
+  $("#fade-out").on("click", function() {
+    $(".murray").slideUp(500).delay(1000).slideDown('slow');
+  });
+  $("#fade-in").on("click", function() {
+    $(".murray").slideDown('fast');
+  });
+  $("#fade-toggle").on("click", function() {
+    $(".murray").fadeToggle('slow');
   });
 
-  $('#fade-in').on('click', function() {
-    $('.murray').fadeIn(200);
+  // ad-hoc animation
+  // $("#murray-box").on("click", ".murray", function() {
+  //   $(this).animate({
+  //     height: '-=50px',
+  //     width: '-=50px'
+  //   }, 500)
+  //   .delay(500)
+  //   .animate({
+  //     height: '+=50px',
+  //     width: '+=50px'
+  //   }, 'fast');
+  // });
+
+  $("#murray-box").on("click", ".murray", function() {
+    // if($(this).hasClass("spin-murray"))
+    $(this).toggleClass("spin-murray");
   });
 
-  $('#fade-toggle').on('click', function() {
-    $('.murray').fadeToggle(500);
-  });
-
-  $('#slide-combo').on('click', function() {
-    $('.murray').slideUp('fast').delay(1000).slideDown('slow');
-  });
-
-  $('#remove-button').on('click', function() {
-    $('.murray').remove();
-  });
-
-  // Animations!
-  // ad-hoc with .animate
-  $('#murray-box').on('click', '.murray', function() {
-    $(this).animate({
-        height: '-=50px',
-        width: '-=50px'
-      }, 500);
-  });
-
-  // Use pre-defined CSS animations by applying a class
-  $('#murray-box').on('click', '.murray', function() {
-    $(this).toggleClass("animate-murray");
-  });
-
-
-  $('#add-button').on('click', function() {
-    var size = randomNumber(200, 500);
-    var img = '<img class="murray" src="http://www.fillmurray.com/' + size + '/' + size + '" />'
-    $('#murray-box').append(img);
+	$("#add-murray").on("click", function() {
+    var size = randomNumber(150, 400);
+    var img = '<img class="murray" src="http://www.fillmurray.com/' + size + '/' + size + '" />';
+    $("#murray-box").append(img);
   });
 
 });
