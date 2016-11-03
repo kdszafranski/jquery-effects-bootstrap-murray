91935aafe828eabf7bbca158a10c7f248e2b1194
Kris Szafranski <kdszafranski@gmail.com>
2016-11-03 10:32:16 -0500

    added in slide effects and buttons

--- app.js
+++ app.js
@@ -9,20 +9,30 @@ $(document).ready(function() {
   $("#fade-toggle").on("click", function() {
     $(".murray").fadeToggle('slow');
   });
+  $("#slide-up").on("click", function() {
+    $(".murray").slideUp(500);
+  });
+  $("#slide-down").on("click", function() {
+    $(".murray").slideDown(200);
+  });
+  $("#slide-combo").on("click", function() {
+    $(".murray").slideUp('fast').delay(1000). slideDown('slow');
+  });
 
-  // ad-hoc animation
-  // $("#murray-box").on("click", ".murray", function() {
-  //   $(this).animate({
-  //     height: '-=50px',
-  //     width: '-=50px'
-  //   }, 500)
-  //   .delay(500)
-  //   .animate({
-  //     height: '+=50px',
-  //     width: '+=50px'
-  //   }, 'fast');
-  // });
+  // ad-hoc animation with .animate()
+  $("#murray-box").on("click", ".murray", function() {
+    $(this).animate({
+      height: '-=50px',
+      width: '-=50px'
+    }, 500)
+    .delay(500)
+    .animate({
+      height: '+=50px',
+      width: '+=50px'
+    }, 'fast');
+  });
 
+  // Use pre-defined CSS class to apply animation effects
   $("#murray-box").on("click", ".murray", function() {
     // if($(this).hasClass("spin-murray"))
     $(this).toggleClass("spin-murray");
